2e59a6f25c850e9fe9fb891a3154d9176513ef7d
Dengliangwen <denglw@si-tech.com.cn>
2020-10-13 10:40:24 +0800

    add plate file

---
+++ mitosis_hack/mitosis-keyboard-basic/config/mitosis.h
@@ -0,0 +1,452 @@
+
+#define HAND_SENSE 12
+#define RIGHT_HAND false
+#define LEFT_HAND true
+
+#define ALPHA_SENSE 20
+#define ALPABETICAL false
+
+// left hand pins
+
+#define L_LED 17
+
+#define L_S01 2
+#define L_S02 5
+#define L_S03 8
+#define L_S04 15
+#define L_S05 14
+#define L_S06 1
+#define L_S07 4
+#define L_S08 7
+#define L_S09 10
+#define L_S10 13
+#define L_S11 0
+#define L_S12 3
+#define L_S13 6
+#define L_S14 9
+#define L_S15 19
+#define L_S16 25
+#define L_S17 29
+#define L_S18 28
+#define L_S19 30
+#define L_S20 24
+#define L_S21 23
+#define L_S22 22
+#define L_S23 21
+#define L_S24 17
+
+#define L_MASK (1<<L_S01 | \
+ 				1<<L_S02 | \
+				1<<L_S03 | \
+				1<<L_S04 | \
+				1<<L_S05 | \
+				1<<L_S06 | \
+				1<<L_S07 | \
+				1<<L_S08 | \
+				1<<L_S09 | \
+				1<<L_S10 | \
+				1<<L_S11 | \
+				1<<L_S12 | \
+				1<<L_S13 | \
+				1<<L_S14 | \
+				1<<L_S15 | \
+				1<<L_S16 | \
+				1<<L_S17 | \
+				1<<L_S18 | \
+				1<<L_S19 | \
+				1<<L_S20 | \
+				1<<L_S21 | \
+				1<<L_S22 | \
+				1<<L_S23 | \
+				1<<L_S24)
+
+// right hand pins
+
+#define R_LED 11
+
+#define R_S01 7
+#define R_S02 4
+#define R_S03 1
+#define R_S04 24
+#define R_S05 28
+#define R_S06 8
+#define R_S07 5
+#define R_S08 2
+#define R_S09 30
+#define R_S10 29
+#define R_S11 9
+#define R_S12 6
+#define R_S13 3
+#define R_S14 0
+#define R_S15 21
+#define R_S16 16
+#define R_S17 13
+#define R_S18 14
+#define R_S19 10
+#define R_S20 15
+#define R_S21 17
+#define R_S22 18
+#define R_S23 19
+#define R_S24 11
+
+#define R_MASK (1<<R_S01 | \
+ 				1<<R_S02 | \
+				1<<R_S03 | \
+				1<<R_S04 | \
+				1<<R_S05 | \
+				1<<R_S06 | \
+				1<<R_S07 | \
+				1<<R_S08 | \
+				1<<R_S09 | \
+				1<<R_S10 | \
+				1<<R_S11 | \
+				1<<R_S12 | \
+				1<<R_S13 | \
+				1<<R_S14 | \
+				1<<R_S15 | \
+				1<<R_S16 | \
+				1<<R_S17 | \
+				1<<R_S18 | \
+				1<<R_S19 | \
+				1<<R_S20 | \
+				1<<R_S21 | \
+				1<<R_S22 | \
+				1<<R_S23 | \
+				1<<R_S24)
+
+// numpad pins
+
+
+#define NUM_S01 18
+#define NUM_S02 19
+#define NUM_S03 22
+#define NUM_S04 23
+#define NUM_S05 17
+#define NUM_S06 16
+#define NUM_S07 25
+#define NUM_S08 24
+#define NUM_S09 10
+#define NUM_S10 7
+#define NUM_S11 2
+#define NUM_S12 30
+#define NUM_S13 9
+#define NUM_S14 8
+#define NUM_S15 1
+#define NUM_S16 0
+#define NUM_S17 5
+#define NUM_S18 5
+#define NUM_S19 5
+#define NUM_S20 5
+#define NUM_S21 5
+#define NUM_S22 5
+#define NUM_S23 5
+#define NUM_S24 5
+
+#define NUM_MASK (1<<NUM_S01 | \
+ 				1<<NUM_S02 | \
+				1<<NUM_S03 | \
+				1<<NUM_S04 | \
+				1<<NUM_S05 | \
+				1<<NUM_S06 | \
+				1<<NUM_S07 | \
+				1<<NUM_S08 | \
+				1<<NUM_S09 | \
+				1<<NUM_S10 | \
+				1<<NUM_S11 | \
+				1<<NUM_S12 | \
+				1<<NUM_S13 | \
+				1<<NUM_S14 | \
+				1<<NUM_S15 | \
+				1<<NUM_S16 | \
+				1<<NUM_S17 | \
+				1<<NUM_S18 | \
+				1<<NUM_S19 | \
+				1<<NUM_S20 | \
+				1<<NUM_S21 | \
+				1<<NUM_S22 | \
+				1<<NUM_S23 | \
+				1<<NUM_S24)
+// 分体Placnk pins Left
+
+#define PLANCK_L_LED 16
+
+#define PLANCK_L_S01 17
+#define PLANCK_L_S02 18
+#define PLANCK_L_S03 19
+#define PLANCK_L_S04 22
+#define PLANCK_L_S05 23
+#define PLANCK_L_S06 24
+#define PLANCK_L_S07 15
+#define PLANCK_L_S08 14
+#define PLANCK_L_S09 13
+#define PLANCK_L_S10 29
+#define PLANCK_L_S11 28
+#define PLANCK_L_S12 25
+#define PLANCK_L_S13 10
+#define PLANCK_L_S14 9
+#define PLANCK_L_S15 8
+#define PLANCK_L_S16 1
+#define PLANCK_L_S17 30
+#define PLANCK_L_S18 0
+#define PLANCK_L_S19 7
+#define PLANCK_L_S20 6
+#define PLANCK_L_S21 5
+#define PLANCK_L_S22 4
+#define PLANCK_L_S23 3
+#define PLANCK_L_S24 2
+
+#define PLANCK_L_MASK (1<<PLANCK_L_S01 | \
+ 				1<<PLANCK_L_S02 | \
+				1<<PLANCK_L_S03 | \
+				1<<PLANCK_L_S04 | \
+				1<<PLANCK_L_S05 | \
+				1<<PLANCK_L_S06 | \
+				1<<PLANCK_L_S07 | \
+				1<<PLANCK_L_S08 | \
+				1<<PLANCK_L_S09 | \
+				1<<PLANCK_L_S10 | \
+				1<<PLANCK_L_S11 | \
+				1<<PLANCK_L_S12 | \
+				1<<PLANCK_L_S13 | \
+				1<<PLANCK_L_S14 | \
+				1<<PLANCK_L_S15 | \
+				1<<PLANCK_L_S16 | \
+				1<<PLANCK_L_S17 | \
+				1<<PLANCK_L_S18 | \
+				1<<PLANCK_L_S19 | \
+				1<<PLANCK_L_S20 | \
+				1<<PLANCK_L_S21 | \
+				1<<PLANCK_L_S22 | \
+				1<<PLANCK_L_S23 | \
+				1<<PLANCK_L_S24)
+
+// 分体Placnk pins Right
+
+#define PLANCK_R_LED 16
+
+#define PLANCK_R_S01 17
+#define PLANCK_R_S02 18
+#define PLANCK_R_S03 19
+#define PLANCK_R_S04 22
+#define PLANCK_R_S05 23
+#define PLANCK_R_S06 24
+#define PLANCK_R_S07 15
+#define PLANCK_R_S08 14
+#define PLANCK_R_S09 13
+#define PLANCK_R_S10 29
+#define PLANCK_R_S11 28
+#define PLANCK_R_S12 25
+#define PLANCK_R_S13 10
+#define PLANCK_R_S14 9
+#define PLANCK_R_S15 8
+#define PLANCK_R_S16 1
+#define PLANCK_R_S17 30
+#define PLANCK_R_S18 0
+#define PLANCK_R_S19 7
+#define PLANCK_R_S20 6
+#define PLANCK_R_S21 5
+#define PLANCK_R_S22 4
+#define PLANCK_R_S23 3
+#define PLANCK_R_S24 2
+
+#define PLANCK_R_MASK (1<<PLANCK_R_S01 | \
+ 				1<<PLANCK_R_S02 | \
+				1<<PLANCK_R_S03 | \
+				1<<PLANCK_R_S04 | \
+				1<<PLANCK_R_S05 | \
+				1<<PLANCK_R_S06 | \
+				1<<PLANCK_R_S07 | \
+				1<<PLANCK_R_S08 | \
+				1<<PLANCK_R_S09 | \
+				1<<PLANCK_R_S10 | \
+				1<<PLANCK_R_S11 | \
+				1<<PLANCK_R_S12 | \
+				1<<PLANCK_R_S13 | \
+				1<<PLANCK_R_S14 | \
+				1<<PLANCK_R_S15 | \
+				1<<PLANCK_R_S16 | \
+				1<<PLANCK_R_S17 | \
+				1<<PLANCK_R_S18 | \
+				1<<PLANCK_R_S19 | \
+				1<<PLANCK_R_S20 | \
+				1<<PLANCK_R_S21 | \
+				1<<PLANCK_R_S22 | \
+				1<<PLANCK_R_S23 | \
+				1<<PLANCK_R_S24)
+
+#ifdef COMPILE_LEFT
+
+#define PIPE_NUMBER 0
+
+#define LED_PIN L_LED
+
+#define S01 L_S01
+#define S02 L_S02
+#define S03 L_S03
+#define S04 L_S04
+#define S05 L_S05
+#define S06 L_S06
+#define S07 L_S07
+#define S08 L_S08
+#define S09 L_S09
+#define S10 L_S10
+#define S11 L_S11
+#define S12 L_S12
+#define S13 L_S13
+#define S14 L_S14
+#define S15 L_S15
+#define S16 L_S16
+#define S17 L_S17
+#define S18 L_S18
+#define S19 L_S19
+#define S20 L_S20
+#define S21 L_S21
+#define S22 L_S22
+#define S23 L_S23
+#define S24 L_S24
+
+#define INPUT_MASK L_MASK
+
+#endif
+
+#ifdef COMPILE_RIGHT
+
+#define PIPE_NUMBER 1
+
+#define LED_PIN R_LED
+
+#define S01 R_S01
+#define S02 R_S02
+#define S03 R_S03
+#define S04 R_S04
+#define S05 R_S05
+#define S06 R_S06
+#define S07 R_S07
+#define S08 R_S08
+#define S09 R_S09
+#define S10 R_S10
+#define S11 R_S11
+#define S12 R_S12
+#define S13 R_S13
+#define S14 R_S14
+#define S15 R_S15
+#define S16 R_S16
+#define S17 R_S17
+#define S18 R_S18
+#define S19 R_S19
+#define S20 R_S20
+#define S21 R_S21
+#define S22 R_S22
+#define S23 R_S23
+#define S24 R_S24
+
+#define INPUT_MASK R_MASK
+
+#endif
+
+
+#ifdef COMPILE_NUMPAD
+
+#define PIPE_NUMBER 2
+
+#define S01 NUM_S01
+#define S02 NUM_S02
+#define S03 NUM_S03
+#define S04 NUM_S04
+#define S05 NUM_S05
+#define S06 NUM_S06
+#define S07 NUM_S07
+#define S08 NUM_S08
+#define S09 NUM_S09
+#define S10 NUM_S10
+#define S11 NUM_S11
+#define S12 NUM_S12
+#define S13 NUM_S13
+#define S14 NUM_S14
+#define S15 NUM_S15
+#define S16 NUM_S16
+#define S17 NUM_S17
+#define S18 NUM_S18
+#define S19 NUM_S19
+#define S20 NUM_S20
+#define S21 NUM_S21
+#define S22 NUM_S22
+#define S23 NUM_S23
+#define S24 NUM_S24
+
+#define INPUT_MASK NUM_MASK
+
+#endif
+
+
+//分体普朗克
+
+#ifdef COMPILE_PLANCK_LEFT
+#define PIPE_NUMBER 4
+#define LED_PIN PLANCK_L_LED
+#define S01 PLANCK_L_S01
+#define S02 PLANCK_L_S02
+#define S03 PLANCK_L_S03
+#define S04 PLANCK_L_S04
+#define S05 PLANCK_L_S05
+#define S06 PLANCK_L_S06
+#define S07 PLANCK_L_S07
+#define S08 PLANCK_L_S08
+#define S09 PLANCK_L_S09
+#define S10 PLANCK_L_S10
+#define S11 PLANCK_L_S11
+#define S12 PLANCK_L_S12
+#define S13 PLANCK_L_S13
+#define S14 PLANCK_L_S14
+#define S15 PLANCK_L_S15
+#define S16 PLANCK_L_S16
+#define S17 PLANCK_L_S17
+#define S18 PLANCK_L_S18
+#define S19 PLANCK_L_S19
+#define S20 PLANCK_L_S20
+#define S21 PLANCK_L_S21
+#define S22 PLANCK_L_S22
+#define S23 PLANCK_L_S23
+#define S24 PLANCK_L_S24
+#define INPUT_MASK PLANCK_L_MASK
+#endif
+
+//分体普朗克 RIGHT
+
+#ifdef COMPILE_PLANCK_RIGHT
+#define PIPE_NUMBER 5
+
+#define LED_PIN PLANCK_R_LED
+
+#define S01 PLANCK_R_S01
+#define S02 PLANCK_R_S02
+#define S03 PLANCK_R_S03
+#define S04 PLANCK_R_S04
+#define S05 PLANCK_R_S05
+#define S06 PLANCK_R_S06
+#define S07 PLANCK_R_S07
+#define S08 PLANCK_R_S08
+#define S09 PLANCK_R_S09
+#define S10 PLANCK_R_S10
+#define S11 PLANCK_R_S11
+#define S12 PLANCK_R_S12
+#define S13 PLANCK_R_S13
+#define S14 PLANCK_R_S14
+#define S15 PLANCK_R_S15
+#define S16 PLANCK_R_S16
+#define S17 PLANCK_R_S17
+#define S18 PLANCK_R_S18
+#define S19 PLANCK_R_S19
+#define S20 PLANCK_R_S20
+#define S21 PLANCK_R_S21
+#define S22 PLANCK_R_S22
+#define S23 PLANCK_R_S23
+#define S24 PLANCK_R_S24
+#define INPUT_MASK PLANCK_R_MASK
+#endif
+
+// Low frequency clock source to be used by the SoftDevice
+#define NRF_CLOCK_LFCLKSRC      {.source        = NRF_CLOCK_LF_SRC_XTAL,            \
+                                 .rc_ctiv       = 0,                                \
+                                 .rc_temp_ctiv  = 0,                                \
+                                 .xtal_accuracy = NRF_CLOCK_LF_XTAL_ACCURACY_20_PPM}
